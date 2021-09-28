57abb2f1a76bf954825c8154254297de6bceb264
GaryFernandoYM <miraclevann224@gmail.com>
2021-09-28 11:55:12 -0500

    nueva actualizacion

--- EstCondicional/EstCondicional.java
+++ EstCondicional/EstCondicional.java
@@ -1,7 +1,12 @@
-import javax.security.auth.x500.X500Principal;
-
 class EstCondicional{
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+      System.out.println("HolA MundO"); 
+      int numero1=20;
+      int numero2=10;
+      if(numero1>numero2){
+        System.out.println("numero mayor es:" + numero1);
+      }else{
+        System.out.println("numero menor es:" + numero2);
+      }
     }
 }
